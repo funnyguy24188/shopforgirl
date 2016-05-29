dc90fce523a5788c38c8184050eaacd89d1b4bf9
david <funnyguy24188@gmail.com>
2016-05-29 18:04:45 +0700

    aa

--- wp-content/themes/virtue/templates/head.php
+++ wp-content/themes/virtue/templates/head.php
@@ -3,6 +3,7 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+<link rel="alternate" href="http://shopforgirl.net/" hreflang="vi-vn" />
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +11,5 @@
   <meta property="fb:app_id" content="508842445974753" />
   <meta property="fb:admins" content="100001811255272"/>
   <meta name="google-site-verification" content="RR03Dt9VoiOJEK0Kyo4z7KqNiCtnVSDu1NS28ZSh5Tw" />
-  <link rel="alternate" href="http://shopforgirl.net/" hreflang="vi-vn" />
   <?php wp_head(); ?>
 </head>
