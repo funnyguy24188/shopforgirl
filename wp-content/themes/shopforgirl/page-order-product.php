91bfd284f3c12f3266d415a6bb5963e3417b2cb9
Triet Le <funnyguy@gmail.com>
2016-06-25 15:19:50 +0700

    update code + update order form

--- wp-content/themes/shopforgirl/page-order-product.php
+++ wp-content/themes/shopforgirl/page-order-product.php
@@ -1,6 +1,7 @@
-<div id="pageheader" class="titleclass">
+<div id="order-page-header" class="titleclass">
     <div class="container">
-        <?php get_template_part('templates/page', 'header'); ?>
+        <h2 style="float:left;margin-top:0">Hóa đơn</h2>
+        <input  type="text" readonly id="total-amount" value="0">
     </div><!--container-->
 </div><!--titleclass-->
 
@@ -12,24 +13,25 @@
                     <form id="order-form" method="post" action="" class="form-horizontal">
                         <fieldset id="customer-info" title="customer-info">
                             <legend>Thông tin khách hàng</legend>
-                            <div class="form-group col-sm-12 customer-basic-info">
-                                <div class="col-sm-6 customer-name-group">
+                            <div class="form-group col-xs-12 col-sm-6 customer-basic-info">
+                                <div class="col-xs-12 col-sm-6 customer-name-group">
                                     <label class="form-label">Tên khách hàng (*)</label>
                                     <input class="form-control" name="customer[name]" required type="text"
                                            placeholder="Nhập tên khách hàng">
                                 </div>
-                                <div class="col-sm-6 customer-phone-group">
+                                <div class="col-xs-12 col-sm-6 customer-phone-group">
                                     <label class="form-label">Số Điện thoại</label>
                                     <input class="form-control" name="customer[phone]" type="text"
                                            placeholder="Nhập số Điện thoại">
                                 </div>
 
                             </div>
-                            <div class="form-group col-sm-12">
+                            <div class="form-group col-xs-12 col-sm-6 customer-address-group">
                                 <label class="form-label">Địa chỉ</label>
                                 <input class="form-control" name="customer[address]" type="text"
                                        placeholder="Nhập địa chỉ">
                             </div>
+                            <div class="clearfix"></div>
                         </fieldset>
                         <fieldset id="product-barcode-scanner" title="product-barcode-scanner">
                             <legend>Mã vạch</legend>
@@ -45,12 +47,14 @@
                         </fieldset>
                         <fieldset id="product-list-show">
                             <legend>Danh sách sản phẩm</legend>
-                            <table class="table">
+                            <table class="table product-list-table">
                                 <thead>
                                 <tr>
                                     <th>STT</th>
                                     <th>Tên sản phẩm</th>
-                                    <th>Số lượng
+                                    <th>Số lượng</th>
+                                    <th>Giá</th>
+                                    <th>Thành tiền</th>
                                     <th>Xóa</th>
                                 </tr>
                                 </thead>
@@ -97,6 +101,26 @@
                     removeProduct(jQuery(this));
                 });
 
+                jQuery("#quantity").keydown(function (e) {
+                    // Allow: backspace, delete, tab, escape, enter and .
+                    if (jQuery.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 ||
+                        // Allow: Ctrl+A
+                        (e.keyCode == 65 && e.ctrlKey === true) ||
+                        // Allow: Ctrl+C
+                        (e.keyCode == 67 && e.ctrlKey === true) ||
+                        // Allow: Ctrl+X
+                        (e.keyCode == 88 && e.ctrlKey === true) ||
+                        // Allow: home, end, left, right
+                        (e.keyCode >= 35 && e.keyCode <= 39)) {
+                        // let it happen, don't do anything
+                        return;
+                    }
+                    // Ensure that it is a number and stop the keypress
+                    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+                        e.preventDefault();
+                    }
+                });
+
 
                 /**
                  * Add product to order list
@@ -105,11 +129,14 @@
                     var name = arg.name;
                     var barcode = arg.barcode.trim();
                     var quantity = parseInt(arg.quantity);
+                    var price = parseInt(arg.regular_price);
                     var addNew = true;
                     var row_pattern = '<tr class="product-item" data-barcode="{barcode}">' +
                         '<td><span class="product-item-num">{product_num}</span></td>' +
                         '<td><input name="product[{barcode}]name" readonly class="product-name"  value="{product_name}"></td>' +
-                        '<td><input name="product[{barcode}]quantity" readonly class="product-quantity" value="{product_quantity}"></td>' +
+                        '<td><input name="product[{barcode}]quanname="product[{barcotity" readonly class="product-quantity" value="{product_quantity}"></td>' +
+                        '<td><input class="product-price" type="text" readonly value="{price}"></td>' +
+                        '<td><input class="product-amount" type="text" readonly value="{amount}"></td>' +
                         '<td><span><i class="fa fa-remove"></i></span></td>' +
                         '</tr>';
 
@@ -125,7 +152,9 @@
                             var html = row_pattern.replace('{product_num}', k + 1)
                                 .replace('{barcode}', barcode)
                                 .replace('{product_name}', name)
-                                .replace('{product_quantity}', quantity);
+                                .replace('{product_quantity}', quantity)
+                                .replace('{price}', price)
+                                .replace('{amount}', price*quantity);
 
                             jQuery(item).replaceWith(html);
                         }
@@ -139,9 +168,14 @@
                         var html = row_pattern.replace('{product_num}', num)
                             .replace(/{barcode}/g, barcode)
                             .replace('{product_name}', name)
-                            .replace('{product_quantity}', quantity);
+                            .replace('{product_quantity}', quantity)
+                            .replace('{price}', price)
+                            .replace('{amount}', price*quantity);
                         jQuery(elem).append(html);
                     }
+
+                    // calculate amount
+                    calculateTotalAmount();
                 }
 
                 /**
@@ -149,8 +183,19 @@
                  */
                 function removeProduct(elem) {
                     jQuery(elem).closest('.product-item').remove();
+                    calculateTotalAmount();
                 }
 
+
+                function calculateTotalAmount() {
+                    var product_list = jQuery('.product-item');
+                    var totalAmount = 0;
+                    jQuery(product_list).each(function (k, item) {
+                        var amountItem = jQuery(item).find('.product-amount').val();
+                        totalAmount += parseInt(amountItem);
+                    });
+                    jQuery('#total-amount').val(totalAmount);
+                }
 
             });
         </script>
