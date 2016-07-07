f1732f8b9118298803cf58568ff2d71be5b0665d
Triet Le <funnyguy@gmail.com>
2016-07-08 00:53:30 +0700

    update code

--- wp-content/themes/shopforgirl/page-google-cloud-print-test.php
+++ wp-content/themes/shopforgirl/page-google-cloud-print-test.php
@@ -1,7 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: david
- * Date: 08/07/2016
- * Time: 00:52
- */
+require_once 'lib/google-cloud-print/GoogleCloudPrint.php';
+
+session_start();
+// Create object
+$gcp = new GoogleCloudPrint();
+$gcp->setAuthToken($_SESSION['accessToken']);
+
+$printers = $gcp->getPrinters();
+print_r($printers);die;
+
+$printerid = "";
+if(count($printers)==0) {
+
+    echo "Could not get printers";
+    exit;
+}
+else {
+
+    $printerid = $printers[0]['id']; // Pass id of any printer to be used for print
+    // Send document to the printer
+    $resarray = $gcp->sendPrintToPrinter($printerid, "Printing Doc using Google Cloud Printing", "./pdf.pdf", "application/pdf");
+
+    if($resarray['status']==true) {
+
+        echo "Document has been sent to printer and should print shortly.";
+    }
+    else {
+        echo "An error occured while printing the doc. Error code:".$resarray['errorcode']." Message:".$resarray['errormessage'];
+    }
+}
+
