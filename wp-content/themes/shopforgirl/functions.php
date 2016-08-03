4069e4119ca49d9ae8b7eefeb31c0d1fb59ba9ea
Triet Le <funnyguy@gmail.com>
2016-08-04 01:39:33 +0700

    update code + syn delete to s3

--- wp-content/themes/shopforgirl/functions.php
+++ wp-content/themes/shopforgirl/functions.php
@@ -6,6 +6,9 @@ require_once 'src/product/SPGProductMod.php';
 require_once 'src/product/SPGProductDetail.php';
 require_once 'src/customer/SPGCustomerDetail.php';
 
+
+
+
 $product_mod = new SPGProductMod();
 $product_mod->init_hook();
 
@@ -25,3 +28,13 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_script('spgScript', get_stylesheet_directory_uri() . '/assets/js/spg_script.js', array('jquery'), '1.0');
 });
 
+// remove the media from s3 when delete from localhost
+add_action('delete_post',function ($post_id){
+    global $as3cf;
+    $post = get_post($post_id);
+    if(!empty($post)) {
+        if($post->post_type == 'attachment') {
+            $as3cf->delete_attachment($post_id);
+        }
+    }
+});
