b00d9338fbd4568eaeb71444460636961d8c1326
Triet Le <funnyguy@gmail.com>
2016-09-26 01:00:01 +0700

    update code + update script

--- wp-content/themes/shopforgirl/assets/js/spg_script.js
+++ wp-content/themes/shopforgirl/assets/js/spg_script.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function () {
     // move commend facebook to after sidebar
-    if (jQuery('.fb-comments') != 'undefined') {
+    if (jQuery('.fb-comments') != 'undefined' && jQruey('.single-post')) {
 
         var fb_comment = jQuery('.fb-comments');
         jQuery('.fb-comments').remove();
