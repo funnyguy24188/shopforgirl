2e0a025b778c21de89619ff7f4cc40ea0cd01fda
Triet Le <funnyguy@gmail.com>
2016-09-26 01:05:16 +0700

    update script

--- wp-content/themes/shopforgirl/assets/js/spg_script.js
+++ wp-content/themes/shopforgirl/assets/js/spg_script.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function () {
     // move commend facebook to after sidebar
-    if (typeof jQuery('.fb-comments') != 'undefined' && typeof jQuery('.single-post') != 'undefined') {
+    if (jQuery('.single-post').length) {
+        if (jQuery('.fb-comments').length) {
 
-        var fb_comment = jQuery('.fb-comments');
-        jQuery('.fb-comments').remove();
-        jQuery('#ktsidebar').closest('#content').append(fb_comment);
+            var fb_comment = jQuery('.fb-comments');
+            jQuery('.fb-comments').remove();
+            jQuery('#ktsidebar').closest('#content').append(fb_comment);
+        }
     }
-
-
 });
