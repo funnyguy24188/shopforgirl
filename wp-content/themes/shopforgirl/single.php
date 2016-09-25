3cb2529d0ec50128518131369782b55d2848cf45
Triet Le <funnyguy@gmail.com>
2016-09-25 20:43:47 +0700

    + update code change order by

--- wp-content/themes/shopforgirl/single.php
+++ wp-content/themes/shopforgirl/single.php
@@ -1,4 +1,4 @@
 <?php get_template_part('templates/content', 'single'); ?>
-<div class="fb-comments" data-href="<?php the_permalink(); ?>" data-numposts="15"  width="100%" data-colorscheme="light"></div>
+<div class="fb-comments" data-href="<?php the_permalink(); ?>" data-numposts="15" order_by="reverse_time" width="100%" data-colorscheme="light"></div>
 
 
