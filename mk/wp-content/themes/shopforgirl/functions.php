c09b08e82583b94c585f89c8ccf2d82171f7a52b
Triet Le <funnyguy@gmail.com>
2016-12-08 05:29:20 +0700

    update code: update master branch

--- mk/wp-content/themes/shopforgirl/functions.php
+++ mk/wp-content/themes/shopforgirl/functions.php
@@ -69,6 +69,7 @@ add_action('wp_enqueue_scripts', function () {
 
 add_action('admin_enqueue_scripts', function () {
     // admin script
+   // wp_enqueue_script('spgInitVars2', get_stylesheet_directory_uri() . '/assets/js/jquery.min.js', null, '1.0', true);
     wp_enqueue_script('spgInitVars', get_stylesheet_directory_uri() . '/assets/js/init-vars.js', array('jquery'), '1.0');
     wp_enqueue_script('spgAdminScript', get_stylesheet_directory_uri() . '/assets/js/admin.js', array('jquery'), '1.0');
 
