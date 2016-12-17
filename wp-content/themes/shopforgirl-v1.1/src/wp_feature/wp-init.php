3df0989ae4827261ee73dce21cfa01830e333cf2
Triet Le <funnyguy@gmail.com>
2016-12-17 20:25:30 +0700

    update code: update add barcode to product list

--- wp-content/themes/shopforgirl-v1.1/src/wp_feature/wp-init.php
+++ wp-content/themes/shopforgirl-v1.1/src/wp_feature/wp-init.php
@@ -1,2 +1,25 @@
 <?php
 add_post_type_support('product', 'excerpt');
+
+add_filter('manage_posts_columns', 'add_barcode_columns_head_product_list', 100, 2);
+add_action('manage_posts_custom_column', 'add_barcode_columns_content_product_list', 10, 2);
+
+function add_barcode_columns_head_product_list($defaults, $post_type)
+{
+    unset($defaults['taxonomy-product_brand']);
+    unset($defaults['gadwp_stats']);
+    if ($post_type == 'product') {
+        $defaults['barcode_field'] = 'BC';
+    }
+    return $defaults;
+}
+
+function add_barcode_columns_content_product_list($column_name, $post_id)
+{
+    if ($column_name == 'barcode_field') {
+        $barcode = get_post_meta($post_id, '_barcode_field', true);
+        if ($barcode) {
+            echo $barcode;
+        }
+    }
+}
